667527d5b19fa81b5a27b146fd2a91e616c68ec8
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-08 13:24:59 +0300

    Add Logout button

--- app/(protected)/settings/page.js
+++ app/(protected)/settings/page.js
@@ -1,10 +1,25 @@
 import { auth } from "@/auth"
+// import { signOut } from "next-auth/react"
 
 const SettingsPage = async () => {
   const session = await auth()
 
+  // const handleLogout = async () => {
+  //   await signOut()
+  // }
+
   return (
-    <div>{JSON.stringify(session)}</div>
+    <div>
+      {JSON.stringify(session)}
+
+      <form className="m-3">
+        <button
+          className="px-4 py-2 text-white"
+        >
+          Log out
+        </button>
+      </form>
+    </div>
   )
 }
 
